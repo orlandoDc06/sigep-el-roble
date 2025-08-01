eb28855161d917269e724c54d5d2004258fd6780
Ever-VC <ever.vasquezc27@gmail.com>
2025-08-01 12:03:49 -0600

    🚚 Rename:  Modificando el nombre de los archivos y tabla de configuración legar

--- app/Models/LegalConfiguration.php
+++ app/Models/LegalConfiguration.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class LegalContiguration extends Model
+class LegalConfiguration extends Model
 {
     //
 }
--- database/migrations/2025_08_01_175859_create_legal_configurations_table.php
+++ database/migrations/2025_08_01_175859_create_legal_configurations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('legal_contigurations', function (Blueprint $table) {
+        Schema::create('legal_configurations', function (Blueprint $table) {
             $table->id();
             $table->decimal('afp_percentage', 5, 2);// % AFP
             $table->decimal('isss_percentage', 5, 2);// % ISSS
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('legal_contigurations');
+        Schema::dropIfExists('legal_configurations');
     }
 };
